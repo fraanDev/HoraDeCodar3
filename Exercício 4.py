e79f7e367ca690c70878a9d4c6839beb18bc5aba
Francielly Menezes <146039068+fraanDev@users.noreply.github.com>
2024-09-20 16:07:23 -0300

    Exercício 4

---
+++ Exercício 4.py
@@ -0,0 +1,10 @@
+
+soma = 0
+contador = 0
+
+for i in range(15, 101):
+ soma += i
+ contador += 1 
+ 
+media = soma / contador
+print("A média aritmética dos números inteiros entre 15 e 100 é", media)
